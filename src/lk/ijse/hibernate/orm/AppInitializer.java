fcd93bcfd2186b973a34402f49afe09d5fe9d6ad
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-07-31 21:20:42 +0530

    entity updated

--- src/lk/ijse/hibernate/orm/AppInitializer.java
+++ src/lk/ijse/hibernate/orm/AppInitializer.java
@@ -2,6 +2,19 @@ package lk.ijse.hibernate.orm;
 
 public class AppInitializer {
     public static void main(String[] args) {
+//Save using save method
+        /*Session session = SessionFactoryConfig.getInstance().getSession();
 
+        Customer customer = new Customer();
+        customer.setId(1);
+        customer.setName("Kamal");
+        customer.setAddress("Galle");
+        customer.setSalary(25000.00);
+
+        Transaction transaction = session.beginTransaction();
+        session.save(customer);
+        transaction.commit();
+
+        session.close();*/
     }
 }

1199836b63577fa0bc3d2c8ef6b1ae9274e5416c
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-08-05 11:05:47 +0530

    add lombok.jar

--- src/lk/ijse/hibernate/orm/entity/Customer.java
+++ src/lk/ijse/hibernate/orm/entity/Customer.java
@@ -1,11 +1,17 @@
 package lk.ijse.hibernate.orm.entity;
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 /*@Entity(name = "customer")*/
-
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "customer")
 public class Customer {
@@ -23,55 +29,4 @@ public class Customer {
     @Column(name = "customer_salary")
     private Double salary;
 
-    public Customer(int id, String name, String address, Double salary) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.salary = salary;
-    }
-
-    public Customer() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 }
